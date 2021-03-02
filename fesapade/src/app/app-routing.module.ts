8eeb000efeeaca8acab47cdae5c0c6b7ac122966
RonaldCoto <ronalcotohndz@gmail.com>
2021-03-02 17:06:27 -0600

    galeria de imagenes lista

--- fesapade/src/app/app-routing.module.ts
+++ fesapade/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LandingpageComponent} from '../app/components/landingpage/landingpage.component'
-
+import {LandingpageComponent} from '../app/components/landingpage/landingpage.component';
+import {CursosComponent} from '../app/components/cursos/cursos.component';
+import {GaleriaComponent} from '../app/components/galeria/galeria.component';
 const routes: Routes = [
     //patmatch para que la ruta coincida completa
     { path: '', redirectTo: '/index', pathMatch: 'full' },
-    { path: 'index', component: LandingpageComponent }
+    { path: 'index', component: LandingpageComponent },
+    {path: 'cursos', component:CursosComponent},
+    {path: 'galeria', component:GaleriaComponent}
 ];
 
 @NgModule({
--- fesapade/src/app/app.module.ts
+++ fesapade/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    CursosComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
