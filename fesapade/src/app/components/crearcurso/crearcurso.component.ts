5717bdb81186aa283042733bf48a2542e90b5d27
adonisarevalo98 <adonisarevalo503@gmail.com>
2021-05-16 12:52:58 -0600

    subida de imagenes para curso

--- fesapade/src/app/components/crearcurso/crearcurso.component.ts
+++ fesapade/src/app/components/crearcurso/crearcurso.component.ts
@@ -21,8 +21,14 @@ export class CrearcursoComponent{
     nombre: null,
     descripcion: null,
    portada: null,
+   imagen1:null,
+   imagen2:null,
+   imagen3:null,
    estado:null,
-   base64textString: null
+   base64textString: null,
+   base64textString1: null,
+   base64textString2: null,
+   base64textString3: null
   }
   
   constructor(public authService: AuthService,private cursoServicio: CursosService) { 
@@ -54,7 +60,7 @@ export class CrearcursoComponent{
     
     }
 
- //evento para subida de portada
+ /************************ evento para subida de portada*******************************/
  seleccionarArchivo(event) {
   var files = event.target.files;
    let file = files[0];
@@ -72,6 +78,59 @@ _handleReaderLoaded(readerEvent) {
   this.curs.base64textString = btoa(binaryString);
 }
 
+/************************ evento para subida de imagen 1 *******************************/
+ seleccionarArchivo1(event) {
+  var files = event.target.files;
+   let file = files[0];
+  this.curs.imagen1 = file.name;
+
+  if(files && file) {
+    var reader = new FileReader();
+    reader.onload = this._handleReaderLoaded1.bind(this);
+    reader.readAsBinaryString(file);
+  }
+}
+
+_handleReaderLoaded1(readerEvent) {
+  var binaryString = readerEvent.target.result;
+  this.curs.base64textString1 = btoa(binaryString);
+}
+
+/************************ evento para subida de imagen 2 *******************************/
+seleccionarArchivo2(event) {
+  var files = event.target.files;
+   let file = files[0];
+  this.curs.imagen2 = file.name;
+
+  if(files && file) {
+    var reader = new FileReader();
+    reader.onload = this._handleReaderLoaded2.bind(this);
+    reader.readAsBinaryString(file);
+  }
+}
+
+_handleReaderLoaded2(readerEvent) {
+  var binaryString = readerEvent.target.result;
+  this.curs.base64textString2 = btoa(binaryString);
+}
+
+/************************ evento para subida de imagen 3 *******************************/
+seleccionarArchivo3(event) {
+  var files = event.target.files;
+   let file = files[0];
+  this.curs.imagen3 = file.name;
+
+  if(files && file) {
+    var reader = new FileReader();
+    reader.onload = this._handleReaderLoaded3.bind(this);
+    reader.readAsBinaryString(file);
+  }
+}
+
+_handleReaderLoaded3(readerEvent) {
+  var binaryString = readerEvent.target.result;
+  this.curs.base64textString3 = btoa(binaryString);
+}
 
 
     //metodo que consume el servicio de cursos para agregar un nuevo curso
@@ -81,7 +140,20 @@ _handleReaderLoaded(readerEvent) {
     if (datos['resultado'] == 'OK') {
     alert(datos['mensaje']);
     
-    this.curs = {id: 0, descripcion: null, nombre: null, portada: null,estado:null,base64textString:null};
+    this.curs = {
+      id: 0,
+      nombre: null,
+      descripcion: null,
+     portada: null,
+     imagen1:null,
+     imagen2:null,
+     imagen3:null,
+     estado:null,
+     base64textString: null,
+     base64textString1: null,
+     base64textString2: null,
+     base64textString3: null
+    }
     }else{
       alert(datos['mensaje']);
     }
