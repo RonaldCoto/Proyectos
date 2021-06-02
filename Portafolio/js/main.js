80589e558e02e5b8559a37cb88ff8cabb104313d
RonaldCoto <ronalcotohndz@gmail.com>
2021-06-02 13:52:49 -0600

    almost done, just miss the projects in line

--- Portafolio/js/main.js
+++ Portafolio/js/main.js
@@ -17,3 +17,15 @@ function toggleskills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleskills)
 })
+
+
+/* show scroll-up */ 
+function scrollUp(){
+    const scrollup = document.getElementById('scroll-up');
+
+    if(this.scrollY >=560)
+        scrollup.classList.add('show-scroll');
+    else
+        scrollup.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
